818a0ea7df80149a19d84386b3cf0dd5f5fb725b
Donovan Murphy <39827226+donniecode1983@users.noreply.github.com>
2021-12-06 08:05:54 -0800

    Setup Gulp to minify HTML, CSS, and JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,53 @@
 // Requires
 const { src, dest, watch, series } = require("gulp");
 const htmlmin = require("gulp-htmlmin");
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const terser = require('gulp-terser');
+// const imagemin = require('gulp-imagemin');
+// const imagewebp = require('gulp-webp');
 
+
+// Minify HTML
 function minHTML() {
   return src("public/**/**/*.html")
     .pipe(htmlmin({ collapseWhitespace: true, removeComments: true }))
-    .pipe(dest("public"));
+    .pipe(dest("production"));
+}
+// Clean and minify CSS
+function compilecss() {
+  return src("public/**/*.css")
+  .pipe(minify())
+  .pipe(dest("production/"))
 }
 
-exports.default = series(minHTML);
+// Minify JS
+function jsmin() {
+  return src("public/**/*.js")
+  .pipe(terser())
+  .pipe(dest("production/"))
+}
+
+// Copy images folder to production
+function copyImgs() {
+  return src("public/**/*.{jpg,png,jpeg}")
+  .pipe(dest("production/"))
+}
+
+// Optimize Images
+// function optimizeimg() {
+//   return src("public/images/**/**/*.{jpg,png}")
+//   .pipe(imagemin([
+//     imagemin.mozjpeg({ quality: 80, progressive: true}),
+//     imagemin.optipng({ optimizationLevel: 2}),
+//   ]))
+//   .pipe(dest("production/assets/images"))
+// }
+
+// Watch Task for CSS
+// function watchTask(){
+//   watch("public/**/*.css", compilecss);
+// }
+
+exports.default = series(minHTML, compilecss, jsmin, copyImgs);
+// series(minHTML, compilecss, jsmin);
