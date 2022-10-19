ae0d9c043bc3850506865d23631ab7bd30145db0
Donovan J Murphy <39827226+donniecode1983@users.noreply.github.com>
2022-10-19 10:53:01 -0500

    added code to create image folder if not present.

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 // const {optimizeImgs} = require("./scripts/optimize-imgs.js");
 const imgOps = require("./scripts/img-ops");
 
+// Create the images folder in assets if it does not exist
+const fs = require('fs')
+const dir = './assets/images';
+
+if(!fs.existsSync(dir)){
+  fs.mkdirSync(dir);
+}
+
 // optimizeImgs("assets/images", "public/assets/images");
 imgOps.process({
     input: "./_raw_images",
     output: "./assets/images",
     jpgQual: 70,
     pngQual: 70,
-    webp: false,
-    maxWidth: 1000,
+    webp: false
+    // maxWidth: 3000,
   });
