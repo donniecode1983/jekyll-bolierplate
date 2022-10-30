894d0c021839ba87ff60834d76fc9ef00e3de594
Donovan J Murphy <39827226+donniecode1983@users.noreply.github.com>
2022-10-30 16:06:17 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,41 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/js/main.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'assets/js')
-    },
-    entry: './src/js/custom.js',
+  entry: "./src/js/main.js",
   output: {
-    path: path.resolve(__dirname, 'assets/js'),
-    filename: 'custom.js',
-    libraryTarget: 'var',
-    library: 'custom'
-  }
-}
+    filename: "main.js",
+    path: path.resolve(__dirname, "assets/js"),
+  },
+  entry: "./src/js/custom.js",
+  output: {
+    path: path.resolve(__dirname, "assets/js"),
+    filename: "custom.js",
+    libraryTarget: "var",
+    library: "custom",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            // This is required for asset imports in CSS, such as url()
+            options: {
+              publicPath: "",
+              // path: path.resolve(__dirname, "assets/css"),
+              filename: "style.css"
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+          // according to the docs, sass-loader should be at the bottom, which
+          // loads it first to avoid prefixes in your sourcemaps and other issues.
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
