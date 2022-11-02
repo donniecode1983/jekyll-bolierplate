b4c205c337f4911d81dfabe103f4bb4186cc2ddf
Donovan J Murphy <39827226+donniecode1983@users.noreply.github.com>
2022-11-02 16:41:04 -0500

    restructured webpack src

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/js/main.js',
+    entry: './webpack/js/main.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'assets/js')
     },
-    entry: './src/js/custom.js',
+    entry: './webpack/js/custom.js',
   output: {
     path: path.resolve(__dirname, 'assets/js'),
     filename: 'custom.js',
